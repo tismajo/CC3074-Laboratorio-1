09c8ef7b79f72992231e89f9b659bc4cabaae3fa
tismajo <gir23559@uvg.edu.gt>
2026-02-06 16:43:12 -0600

    extras 5.3 y 5.4

--- extras.py
+++ extras.py
@@ -32,7 +32,7 @@ print("Correlación productionCoAmount vs revenue:", round(corr_prodco, 3))
  """
 
 # Proporción femenina en el elenco
-df["femaleRatio"] = df["castWomenAmount"] / (
+""" df["femaleRatio"] = df["castWomenAmount"] / (
     df["castWomenAmount"] + df["castMenAmount"]
 )
 
@@ -49,4 +49,39 @@ plt.show()
 
 # Correlación
 corr_gender = df[["actorsAmount", "femaleRatio"]].corr().iloc[0,1]
-print("Correlación actoresAmount vs femaleRatio:", round(corr_gender, 3))
+print("Correlación actoresAmount vs femaleRatio:", round(corr_gender, 3)) """
+
+# 5.3¿Las películas con bajo presupuesto pueden alcanzar alta popularidad?
+""" plt.figure(figsize=(7,5))
+sns.scatterplot(
+    data=df,
+    x="budget",
+    y="popularity"
+)
+plt.xscale("log")
+plt.title("Presupuesto vs Popularidad")
+plt.xlabel("Presupuesto (escala log)")
+plt.ylabel("Popularidad")
+plt.show()
+
+# Correlación
+corr_budget_pop = df[["budget", "popularity"]].corr().iloc[0,1]
+print("Correlación budget vs popularity:", round(corr_budget_pop, 3))
+ """
+
+# 5.4 ¿Existen diferencias en la duración promedio según el género principal?
+""" genre_runtime = (
+    df.groupby("genres")["runtime"]
+      .mean()
+      .sort_values(ascending=False)
+)
+
+plt.figure(figsize=(9,5))
+genre_runtime.head(10).plot(kind="bar")
+plt.title("Duración promedio por género principal")
+plt.xlabel("Género")
+plt.ylabel("Duración promedio (min)")
+plt.show()
+
+print(genre_runtime.head(10)) """
+
