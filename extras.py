9005e721ea83dc25bd398b64a339c638fa23f798
Leonardo Mejía <leomejia646@gmail.com>
2026-02-06 16:39:23 -0600

    feat: extra 2

--- extras.py
+++ extras.py
@@ -12,7 +12,7 @@ df = pd.read_csv("movies_2026.csv", sep=",", encoding="latin1")
 
 #¿Existe relación entre el número de compañías productoras y el desempeño comercial?
 # Relación entre número de productoras e ingresos
-prodco_revenue = (
+""" prodco_revenue = (
     df.groupby("productionCoAmount")["revenue"]
         .mean()
         .dropna()
@@ -29,3 +29,24 @@ plt.show()
 corr_prodco = df[["productionCoAmount", "revenue"]].corr().iloc[0,1]
 print("Correlación productionCoAmount vs revenue:", round(corr_prodco, 3))
 
+ """
+
+# Proporción femenina en el elenco
+df["femaleRatio"] = df["castWomenAmount"] / (
+    df["castWomenAmount"] + df["castMenAmount"]
+)
+
+plt.figure(figsize=(7,5))
+sns.scatterplot(
+    data=df,
+    x="actorsAmount",
+    y="femaleRatio"
+)
+plt.title("Cantidad de actores vs proporción femenina")
+plt.xlabel("Cantidad de actores")
+plt.ylabel("Proporción de mujeres")
+plt.show()
+
+# Correlación
+corr_gender = df[["actorsAmount", "femaleRatio"]].corr().iloc[0,1]
+print("Correlación actoresAmount vs femaleRatio:", round(corr_gender, 3))
